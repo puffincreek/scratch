adef9a1df020dbd93ec2b7d29775db6bfb464b29
maggu <maggu@puffincreek.org>
2023-09-26 16:52:55 -0400

    Initial Commit

--- redis-demo/src/main/java/org/example/Console.java
+++ redis-demo/src/main/java/org/example/Console.java
@@ -13,10 +13,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 import redis.clients.jedis.HostAndPort;
-import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.util.SafeEncoder;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -26,30 +27,80 @@ public class Console implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(final String... args) throws Exception {
         String host = readRedisHostFromStdIn(args);
         if (!StringUtils.hasLength(host)) {
             log.error("no host supplied. Default to localhost");
             host = "localhost";
         }
 
-        testTlsConnection(host);
+        testTlsPool(host);
+//        testTlsConnection(host);
     }
 
-    private void testTlsConnection(String host) {
-        final GenericObjectPoolConfig<?> config = new GenericObjectPoolConfig<>();
-        config.setMaxTotal(10);
-
-        final Set<HostAndPort> nodes = new HashSet<>();
-        nodes.add(new HostAndPort(host, 6379));
-
-        log.info("testing redis ops");
-        try (final JedisCluster jedisCluster = new JedisCluster(nodes, 1000, 100, 1, null, "my-client", config, true)) {
-            jedisCluster.set("some-key", "some-value");
-            log.info("result: {}", jedisCluster.get("some-key"));
-        } catch (final Exception e) {
-            log.error("Root cause",  e.getCause());
+    static class LoggingJedisPool extends JedisPool {
+        public LoggingJedisPool(final HostAndPort hostAndPort) {
+            super(new GenericObjectPoolConfig<>(),
+                    hostAndPort.getHost(),
+                    hostAndPort.getPort(),
+                    2000,
+                    null,
+                    null,
+                    true);
         }
+
+        @Override
+        public Jedis getResource() {
+            try {
+                return internalPool.borrowObject();
+            } catch (final Exception e) {
+                log.error("Jedis exception", e);
+            }
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void testTlsPool(final String configHost) {
+        final HostAndPort configHostNPort = new HostAndPort(configHost, 6379);
+        try (final JedisPool pool = new LoggingJedisPool(configHostNPort)) {
+            final Jedis jedis = testConnection(configHostNPort, pool);
+            final List<Object> slots = jedis.clusterSlots();
+
+            for (final Object slotInfoObj : slots) {
+                List<Object> slotInfo = (List<Object>) slotInfoObj;
+                if (slotInfo.size() <= 2) {
+                    continue;
+                }
+
+                final List<Object> hostParameters = (List<Object>) slotInfo.get(2);
+                if (hostParameters.isEmpty()) {
+                    continue;
+                }
+
+                final HostAndPort node = generateHostAndPort(hostParameters);
+                try (final JedisPool nodePool = new LoggingJedisPool(node)) {
+                    testConnection(node, nodePool);
+                }
+            }
+        }
+    }
+
+    private HostAndPort generateHostAndPort(final List<Object> hostParameters) {
+        String host = SafeEncoder.encode((byte[]) hostParameters.get(0));
+        int port = ((Long) hostParameters.get(1)).intValue();
+        return new HostAndPort(host, port);
+    }
+
+    private Jedis testConnection(final HostAndPort hostAndPort,
+                                 final JedisPool pool) {
+        final Jedis jedis = pool.getResource();
+        if (jedis == null) {
+            log.error("no connection established towards: {}", hostAndPort);
+        } else {
+            log.error("connection established towards: {}", hostAndPort);
+        }
+        return jedis;
     }
 
     private String readRedisHostFromStdIn(final String[] args) throws ParseException {
@@ -68,4 +119,5 @@ public class Console implements CommandLineRunner {
 
         return null;
     }
+
 }
